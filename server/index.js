60226db7a005de3ca1265794d72a5a5c4d992539
Ziyauddin <57740414+Ziyauddinshah@users.noreply.github.com>
2021-10-31 03:39:27 +0530

    updated some feature
    updated and added new feature

--- server/index.js
+++ server/index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const mysql = require("mysql");
 const cors = require("cors");
-// "nodemon": "^2.0.12"
-// "devstart": "nodemon index.js",
+
+// npm run devstart make node server live
+// "test": "echo \"Error: no test specified\" && exit 1"
+
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
 const bodyParser = require("body-parser");
@@ -13,23 +15,12 @@ const session = require("express-session");
 app.use(express.json());
 app.use(cors({
     origin: ["http://localhost:3000"],
-    methods: ["GET","POST"],
-    credentials: true
+    methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH'],
+    credentials:true, //access-control-allow-credentials:true
+    optionSuccessStatus:200
 }));
 app.use(cookieparser());
 // app.use(bodyParser.urlencoded({extended: true}));
-
-// app.use(
-//     session({
-//         key: "user",
-//         secret: "subscribe",
-//         resave: false,
-//         saveUninitialized: false,
-//         cookie:{
-//             expire: 60 * 60 * 5,
-//         },
-//     })
-// );
 
 const db = mysql.createConnection({
     host: "localhost",
@@ -39,6 +30,7 @@ const db = mysql.createConnection({
     port: "3308"
 });
 
+// checking db connection
 db.connect((error) => {
     if(error)
         throw error;
@@ -47,26 +39,22 @@ db.connect((error) => {
 
 // show all posts
 app.get("/homepage/showpost", (req,res) => {
-    const sqlSelect = "SELECT * FROM posts ";
+    const sqlSelect = "SELECT * FROM posts ORDER BY post DESC";
     db.query(sqlSelect,(error,result) =>{
-        // console.log(result);
         res.send(result);
     })
 })
 
 // show all comment
-app.post("/homepage", (req1,res1) => {
-    const post = req1.body.post;
-    // console.log(post1);
-    const sqlSelect = "SELECT * FROM commentonpost WHERE post = ?";
-    db.query(sqlSelect,[post],(error,result) =>{
-        // console.log(error)
-        // throw result;
+app.post("/homepage/showcomment", (req1,res1) => {
+    const postid = req1.body.PostId;
+    const sqlSelect = "SELECT * FROM commentonpost WHERE postid = ?";
+    db.query(sqlSelect,[postid],(error,result) =>{
         var string=JSON.stringify(result);
         // console.log(string);
         var json =  JSON.parse(string);
-        // to get one value here is the option
-        console.log(json);
+        //// to get one value here is the option
+        // console.log(json);
         // console.log(result);
         res1.send(json);
     })
@@ -89,13 +77,33 @@ app.post("/homepage/addpost",(request,response) =>{
     })
 })
 
+//deleting post
+app.delete("/deletepost/:postid",(request,response) =>{
+    const deletepostid = request.params.postid;
+    // console.log(deletepostid);
+    const DeletePost = "DELETE from posts where postid = ?";
+    db.query(DeletePost,[deletepostid],(err,result) => {
+        if(err) {
+            console.log(error)
+            // response.send(err);
+        }
+        else{
+            console.log('Number of rows deleted = ' + result.affectedRows);
+            // console.log(result);
+            // response.send(result);
+        }
+    });
+    // window.location.reload(false);
+})
+
 // adding comment
 app.post("/homepage/addcomment",(request,response) => {
-    const post = request.body.post;
+    const postid = request.body.postid;
     const username = request.body.username;
     const comment = request.body.comment;
-    const InsertPost = "INSERT INTO commentonpost(username,post,comment) VALUES(?,?,?)";
-    db.query(InsertPost,[username,post,comment],(err,result) => {
+    // console.log(postid,username,comment);
+    const InsertPost = "INSERT INTO commentonpost(username,postid,comment) VALUES(?,?,?)";
+    db.query(InsertPost,[username,postid,comment],(err,result) => {
         if(err) {
             // console.log(error)
             response.send(err);
@@ -127,16 +135,7 @@ app.post("/registerpage" ,(request,response) => {
     });
 })
 
-// app.get("/login", (req,res) => {
-//     if(req.session.user) {
-//         res.send({loggedIn: true, user: req.session.user});
-//     }
-//     else{
-//         res.send({loggedIn: false});
-//     }
-// });
-
-
+//login process
 app.post("/loginpage" ,(request,response) => {
     const name = request.body.emailoruserid;
     const email = name;
@@ -161,13 +160,6 @@ app.post("/loginpage" ,(request,response) => {
     });
 });
 
-// app.get('/', (request,response) => {
-//     const sqlInsert = "INSERT INTO movies (movie_name, movie_review) VALUES('yodha', 'good');";
-//     db.query(sqlInsert, (error,result) =>{
-//         response.send("Inserted");
-//     })
-    
-// })
 app.listen(3001, () =>{
     console.log("running on port 3001");
 })
